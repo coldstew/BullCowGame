936c1729815d4683f67466216f83c596fa8275ce
jihwanahn <k1tstw@tutanota.com>
2020-12-17 01:34:11 +0900

    Discarded .idea and some codes added

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,21 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
 	Super::BeginPlay();
 
-	PrintLine("Welcome to the Bull Cows Game!\nPress Enter to continue...");
-	PrintLine("");
+	PrintLine(TEXT("Welcome to the Bull Cows Game!"));
+	PrintLine(TEXT("Guess the %d letter word!"), difficulty);
+	PrintLine(TEXT("Press Enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
 	ClearScreen();
-	FString HiddenWord;
+	FString HiddenWord = TEXT("cake");
+	if (Input == HiddenWord)
+	{
+		PrintLine(TEXT("You have Won!"));
+	}
+	else
+	{
+		PrintLine(TEXT("You have Lost!"));
+	}
 }
--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -15,7 +15,9 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 	
+
 	// Your declarations go below!
 	private:
-	
+	UPROPERTY()
+		int32 difficulty = 4;
 };
