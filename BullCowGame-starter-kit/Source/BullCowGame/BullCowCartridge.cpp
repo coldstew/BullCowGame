c6aed4425b9728649e0f99fb3264e4d8b0a0bc7d
jihwanahn <k1tstw@tutanota.com>
2020-12-23 23:56:09 +0900

    Some changes

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,22 +4,41 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
 	Super::BeginPlay();
+	//
 
 	PrintLine(TEXT("Welcome to the Bull Cows Game!"));
 	PrintLine(TEXT("Guess the %d letter word!"), difficulty);
-	PrintLine(TEXT("Press Enter to continue..."));
+	PrintLine(TEXT("Press Tab to continue..."));
+
+	InitGame();
+	// TODO : I need to prompt player for guess
 }
+
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
 	ClearScreen();
-	FString HiddenWord = TEXT("cake");
 	if (Input == HiddenWord)
 	{
 		PrintLine(TEXT("You have Won!"));
 	}
 	else
 	{
-		PrintLine(TEXT("You have Lost!"));
+		if (Input.Len() != HiddenWord.Len() && 0 < lives)
+		{
+			PrintLine(TEXT("The Hidden Word is %d characters long, try again!"), HiddenWord.Len());
+			PrintLine(TEXT("%d Lives left!"), lives);
+		}
+		else 
+		{
+			PrintLine(TEXT("You have Lost!"));
+		}
+		--lives;
 	}
+}
+
+void UBullCowCartridge::InitGame()
+{
+	HiddenWord = TEXT("cake");
+	lives = 5;
 }
